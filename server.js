85af032a8f8b5c762cce9ef0b2d8fc6fe886213e
brettDNE <brett@digitalnextera.com>
2025-08-29 10:38:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.post('/transcript', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Transcript service running on :${PORT}`));
